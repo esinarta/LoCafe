fd83e502cc11a5d4e23f57fe947c6e5f1a636eb4
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2020-11-23 16:42:24 -0800

    can delete favourites but kind of janky, list doesn't refresh properly

--- app/src/main/java/ca/bcit/locafe/ListArrayAdapter.java
+++ app/src/main/java/ca/bcit/locafe/ListArrayAdapter.java
@@ -5,12 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 import ca.bcit.locafe.ui.favourites.FavouriteItem;
 
 public class ListArrayAdapter extends BaseAdapter {
+    FirebaseAuth firebaseAuth;
     private Context context;
     private ArrayList<FavouriteItem> arrayList;
     private TextView name;
@@ -31,10 +39,29 @@ public class ListArrayAdapter extends BaseAdapter {
         return position;
     }
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         convertView = LayoutInflater.from(context).inflate(R.layout.favourites_row_layout, parent, false);
         name = convertView.findViewById(R.id.favourite_name);
         name.setText(arrayList.get(position).getName());
+
+        Button delete = convertView.findViewById(R.id.btnDeleteFav);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth = FirebaseAuth.getInstance();
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                final String userId = currentUser.getUid();
+
+                DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(userId);
+                final DatabaseReference favouritesRef = userRef.child("favourites");
+                favouritesRef.child(arrayList.get(position).getKey()).removeValue();
+
+                arrayList.remove(position);
+                notifyDataSetChanged();
+            }
+        });
+
         return convertView;
     }
 }
